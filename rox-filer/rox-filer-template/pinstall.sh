4d73c3de50bb7b7120ccc1fe496a66f07bd7dddc
01micko <01micko@gmail.com>
2017-06-07 16:56:10 +1000

    update rox-filer

--- rox-filer/rox-filer-template/pinstall.sh
+++ rox-filer/rox-filer-template/pinstall.sh
@@ -9,7 +9,7 @@ if [ "`pwd`" != "/" ];then
 
 #120521 better way to create OpenWith entries (will have icons in menu)...
 #120601 filter out mtPaint-snapshot-screen-capture.desktop ...
-for ONEOPEN in Abiword Bcrypt ePDFView FFConvert Firefox Geany gedit evince gpicview mtpaint Ghasher Ghostview Gnumeric Gxine HomeBank InkLite Inkscape Pburn ISOMaster Leafpad Mozilla-Firefox mhWaveEdit Notecase Opera Planner Pmusic PupZip SeaMonkey-Composer SeaMonkey-web-browser Viewnior XArchive
+for ONEOPEN in Abiword Bcrypt ePDFView Firefox Geany gedit evince gpicview mtpaint Ghasher Ghostview gnome-mplayer Gnumeric Gxine HomeBank InkLite Inkscape Pburn ISOMaster Leafpad Mozilla-Firefox mhWaveEdit Notecase Opera Planner Pmusic PupZip SeaMonkey-Composer SeaMonkey-web-browser Viewnior XArchive
 do
  FNDOPEN="`find usr/share/applications -mindepth 1 -maxdepth 1 -type f -iname "${ONEOPEN}*.desktop" | head -n 1`"
  [ "$FNDOPEN" ] && ln -snf /${FNDOPEN} root/.config/rox.sourceforge.net/OpenWith/${ONEOPEN}

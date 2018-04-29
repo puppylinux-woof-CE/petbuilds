d179dfa6be7a9688a102ffd62136dd0a1f06a48e
woodenshoe-wi <woodenshoe-wi@users.noreply.github.com>
2018-04-28 21:37:37 -0500

    Update rox-filer petbuild
    Source code is now at github with patches already applied.
    Also merged dtomas and seirios forks, cherry-picked some commits
    from jun7 along with my own changes.

--- petbuilds-run_woof.sh
+++ petbuilds-run_woof.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-COMMON32_ISO_URL="https://github.com/woodenshoe-wi/common32/releases/download/v0.0.1-alpha/common32-0.0.1.iso"
-COMMON32_DEVX_URL="https://github.com/woodenshoe-wi/common32/releases/download/v0.0.1-alpha/devx_common32_0.0.1.sfs"
+COMMON32_ISO_URL="https://github.com/woodenshoe-wi/common32/releases/download/v0.0.2/common32-0.0.2.iso"
+COMMON32_DEVX_URL="https://github.com/woodenshoe-wi/common32/releases/download/v0.0.2/devx_common32_0.0.2.sfs"
 
-COMMON64_ISO_URL="https://github.com/woodenshoe-wi/common64/releases/download/v0.0.1/common64-0.0.1.iso"
-COMMON64_DEVX_URL="https://github.com/woodenshoe-wi/common64/releases/download/v0.0.1/devx_common64_0.0.1.sfs"
+COMMON64_ISO_URL="https://github.com/woodenshoe-wi/common64/releases/download/v0.0.2/common64-0.0.2.iso"
+COMMON64_DEVX_URL="https://github.com/woodenshoe-wi/common64/releases/download/v0.0.2/devx_common64_0.0.2.sfs"
 
 # autodetect HOSTARCH - how hard can it be?
 case `uname -m` in
